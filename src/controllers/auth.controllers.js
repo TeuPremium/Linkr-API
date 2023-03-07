7dda074fde5e2ce854ff3059e396e9e733ec45cc
breno-aredes <breno_mg10@hotmail.com>
2023-03-06 23:18:44 -0300

    feat: controller signup created

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,6 +2,22 @@ import { db } from "../database/database.connection.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export async function signup(req, res) {}
+export async function signup(req, res) {
+  const { name } = req.body;
+  const { email, password } = res.locals.user;
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  try {
+    await db.query(
+      `INSERT INTO users (name, email, password) VALUES ($1, $2 , $3);`,
+      [name, email, hashedPassword]
+    );
+
+    res.status(201).send("Conta criada com sucesso.");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
 
 export async function signin(req, res) {}
