d9a74e323ed4862315a65af8cfff0da9dbab41fe
breno-aredes <breno_mg10@hotmail.com>
2023-03-06 23:12:55 -0300

    feat: auth.validate created

--- src/middlewares/auth.validate.js
+++ src/middlewares/auth.validate.js
@@ -2,8 +2,29 @@ import { db } from "../database/database.connection.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export function authToken(req, res, next) {}
+export async function singupValidate(req, res, next) {
+  const { email, password, consfirmPassword } = req.body;
 
-export function singupValidate(req, res, next) {}
+  if (password !== consfirmPassword) {
+    return res.status(422).send("As senha são diferentes.");
+  }
 
-export function singinValidate(req, res, next) {}
+  try {
+    const userExist = await db.query(`SELECT * FROM users WHERE email = $1`, [
+      email,
+    ]);
+
+    if (userExist.rowCount) {
+      return res.status(409).send("Usuário já cadastrado");
+    }
+
+    res.locals.user = { email, password };
+    next();
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function singinValidate(req, res, next) {}
+
+export async function authToken(req, res, next) {}
