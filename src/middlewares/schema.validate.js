386df2be00b46ebd98c4c01d71ce8d1892279c5d
breno-aredes <breno_mg10@hotmail.com>
2023-03-06 23:03:27 -0300

    feat: schema.validate created

--- src/middlewares/schema.validate.js
+++ src/middlewares/schema.validate.js
@@ -1,1 +1,11 @@
-export function schemaValidate(schema) {}
+export function schemaValidate(schema) {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body, { abroutEarly: false });
+
+    if (error) {
+      const err = error.details.map((err) => err.message);
+      return res.status(422).send(err);
+    }
+    next();
+  };
+}
