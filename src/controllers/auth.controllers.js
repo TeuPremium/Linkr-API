f8c79b1271967110192952061e208e8b77d239ca
breno-aredes <breno_mg10@hotmail.com>
2023-03-06 22:57:10 -0300

    feat: base files for auth created

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,7 @@
+import { db } from "../database/database.connection.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export async function signup(req, res) {}
+
+export async function signin(req, res) {}
---
+++ src/middlewares/auth.validate.js
@@ -0,0 +1,9 @@
+import { db } from "../database/database.connection.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export function authToken(req, res, next) {}
+
+export function singupValidate(req, res, next) {}
+
+export function singinValidate(req, res, next) {}
