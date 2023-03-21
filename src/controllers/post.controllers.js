3c0cdf272c0891f20c2fe2be0ebbc7a1cfbf9621
Vini <vinicius.brandaot@gmail.com>
2023-03-21 15:56:45 -0300

    scrape metadata

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -1,4 +1,8 @@
 import { db } from "../database/database.connection.js";
+import axios from "axios"
+import * as cheerio from 'cheerio';
+import fetch from "node-fetch";
+
 
 export async function addPost(req, res) {
   try {
@@ -35,17 +39,25 @@ export async function deletePost(req, res) {
 
 export async function getPost(req, res) {
   try {
+    
     const getPosts = await db.query(`
-        SELECT users.username, users.image, posts.url, posts.comment, users.id, posts.id as "postId" 
-        FROM users, posts 
-        WHERE users.id = posts."userId"
-        ORDER BY posts.id
-        DESC LIMIT 20 
-        `);
-
-    console.log(getPost.rows);
-    return res.status(200).send(getPosts.rows);
-  } catch (error) {
+    SELECT users.username, users.image, posts.url, posts.comment, users.id, posts.id as "postId" 
+    FROM users, posts 
+    WHERE users.id = posts."userId"
+    ORDER BY posts.id
+    DESC LIMIT 20 
+    `);
+    const urlData = []
+    let url
+    for (let index = 0; index < getPosts.rows.length; index++) {
+      let url = getPosts.rows[index].url
+      const element = await scrapeData(url);
+      getPosts.rows[index].urlData = element
+    }
+  
+        
+        return res.status(200).send(getPosts.rows);
+      } catch (error) {
     if (error.detail) {
       return res.status(500).send(error.detail);
     } else {
@@ -53,6 +65,32 @@ export async function getPost(req, res) {
     }
   }
 }
+
+async function scrapeData(url){
+ 
+  
+  // console.log("piu")
+  // const data = axios(aaa)
+  try {
+    const response = await fetch(url);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    const title = $('title').text();
+    const description = $('meta[name="description"]').attr('content');
+    const image = $('meta[property="og:image"]').attr('content');
+
+    return ({
+      title,
+      description,
+      image
+    });
+
+  } catch (error) {
+    console.error(error);
+    return ({title: null, description: null, image: null})
+  }
+
+} 
 
 export async function getUserPosts(req, res) {
   const { id } = req.params;
@@ -68,8 +106,6 @@ export async function getUserPosts(req, res) {
         `,
       [id]
     );
-
-    console.log(getPost.rows);
     return res.status(200).send(getPosts.rows);
   } catch (error) {
     if (error.detail) {
@@ -79,6 +115,10 @@ export async function getUserPosts(req, res) {
     }
   }
 }
+
+
+
+
 
 export async function editPost(req, res) {
   try {
