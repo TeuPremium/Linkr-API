0a9a94f441e800abce788e4979d93044a1f7c393
breno-aredes <breno_mg10@hotmail.com>
2023-03-23 18:13:37 -0300

    feat: controller delete likes created

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -4,14 +4,14 @@ import { db } from "../database/database.connection.js";
 export async function getLikes(req, res) {
   const { postId } = req.params.postId;
   try {
-    likedUsersId = db.query(
+    const result = await db.query(
       `SELECT users.name FROM likes
          JOIN users ON likes."userId" = users.id
-         WHERE likes."postId" = $1`,
+         WHERE likes."postId" = $1;`,
       [postId]
     );
-    likedUser = result.rows.map((e) => e.name);
-    res.send({ likedUser, count: likedUser.length }).status(200);
+    const likedUsers = result.rows.map((e) => e.name);
+    res.send({ likedUsers, count: likedUsers.length }).status(200);
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -22,7 +22,7 @@ export async function postLike(req, res) {
   const { userId, postId } = req.body;
 
   try {
-    await db.query(`INSERT INTO likes ("userId", "PostId") VALUES ($1, $2)`, [
+    await db.query(`INSERT INTO likes ("userId", "postId") VALUES ($1, $2);`, [
       userId,
       postId,
     ]);
@@ -33,4 +33,16 @@ export async function postLike(req, res) {
 }
 
 //deletar o like
-export async function deleteLike(req, res) {}
+export async function deleteLike(req, res) {
+  const { userId, postId } = req.body;
+
+  try {
+    await db.query(`DELETE FROM likes ("userId", "postId) VALUES ($1, $2) ;`, [
+      userId,
+      postId,
+    ]);
+    res.sendStatus(202);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
