9c93b02d7617eb4129afa8c69955d21bd8c2715c
breno-aredes <breno_mg10@hotmail.com>
2023-03-23 17:03:36 -0300

    feat: route like created

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@ import cors from "cors";
 import { authRouter } from "./routes/auth.routes.js";
 import { postRouter } from "./routes/postRoutes.js";
 import { hashtagRank } from "./routes/rankHashtagroute.js";
-
+import { likeRouter } from "./routes/likes.routes.js";
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-server.use([authRouter, postRouter, hashtagRank]);
+server.use([authRouter, postRouter, hashtagRank, likeRouter]);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, console.log(`servidor conectado na porta: ${PORT}`));
