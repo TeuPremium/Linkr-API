2f706091ab7a723c8c6611dc63a1e202dcd28b84
Alan Timbó <alancorreiatimbo@gmail.com>
2023-03-25 00:31:00 -0300

    feat: create user route

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@ import cors from "cors";
 import { authRouter } from "./routes/auth.routes.js";
 import { postRouter } from "./routes/postRoutes.js";
 import { hashtagRank } from "./routes/rankHashtagroute.js";
-
+import { userRouter } from "./routes/user.routes.js";
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-server.use([authRouter, postRouter, hashtagRank]);
+server.use([authRouter, postRouter, hashtagRank, userRouter]);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, console.log(`servidor conectado na porta: ${PORT}`));
